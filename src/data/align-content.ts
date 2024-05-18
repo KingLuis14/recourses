75369da1f578bd8176b0863a2b8e4f77ad23a86f
Luis Enrique <enriquecalero284@gmail.com>
2024-05-17 22:45:36 -0500

    =Practica 1 lista

---
+++ src/data/align-content.ts
@@ -0,0 +1,5 @@
+const alignContent = [
+    "flex-start","center","flex-end","stretch","baseline"
+]
+
+export default alignContent;
---
+++ src/data/justify-content.ts
@@ -0,0 +1,5 @@
+const justifyContent = [
+    "flex-start","center","flex-end","space-between","space-around","space-evenly"
+]
+
+export default justifyContent;
